5bfc55b81bdd7e66f3a83e935b39a23b0cd839aa
Mehmet Tosun <m.tosun@mebura.nl>
2020-04-18 10:34:53 +0200

    Updates

---
+++ precache-manifest.ee1bac37ba3ce2192a3a3d03836e9dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42297d93ed1e86047fc9d8917738c00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c65b17ac3a4c6241b62d",
+    "url": "/monsters-rolodex/static/css/main.ab530bac.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65b17ac3a4c6241b62d",
+    "url": "/monsters-rolodex/static/js/main.418d9dc5.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
